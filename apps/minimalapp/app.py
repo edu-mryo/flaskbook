06ad55e0ade6089b79f990c4751e12d3fd9e541c
edu-mryo <edu.mryo@gmail.com>
2023-02-02 23:32:46 +0800

    first commit

---
+++ apps/minimalapp/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route(
+    "/",
+)
+def index():
+    return "Hello, Flaskbook!"
+
+
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello, {name}"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
