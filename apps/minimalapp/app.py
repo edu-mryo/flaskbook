e35664d34430fb3526430ce5f95208778ec0c93c
markryo <dev.mryo@gmail.com>
2023-02-16 09:15:40 +0800

    Adding validation for forms

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, redirect, render_template, request, url_for
+from email_validator import EmailNotValidError, validate_email
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+
+# Adding SECRET_KEY
+
+app.config["SECRET_KEY"] = "7-dRj?'RPwQ6:KEapv$&nvMZMmh"
 
 
 @app.route(
@@ -27,9 +32,45 @@ def contact():
 #  permitting GET and POST 
 
 
-@app.route("/contact/complete", methods=["GET", "POST"])  
+@app.route("/contact/complete", methods=["GET", "POST"])
 def contact_complete():
     if request.method == "POST":
+        # get form input by request.form
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        # check inputs
+
+        is_valid = True
+
+        if not username:
+            flash("Please fill in the Username")
+            is_valid = False
+        
+        if not email:
+            flash("Please fiill in the Email")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("Please fill in a valid Email")
+            is_valid = False
+
+        if not description:
+            flash("Please fill in the contact description")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
+
+        # Send email
+
+        # Redirect to Contact Complete Endpoint
+
+        flash("Thank you for contacting us")
         return redirect(url_for("contact_complete"))
 
     # if GET, render contact_complete.html    
